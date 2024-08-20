4b522c1273e947b1605ea84898071fddf8b22137
empform <softsince1997@gmail.com>
2024-08-20 03:27:09 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,3 @@
-document.getElementById('nextToPage2').addEventListener('click', function() {
-    window.location.href = 'page2.html';
-});
-
-document.getElementById('nextToPage3').addEventListener('click', function() {
-    window.location.href = 'page3.html';
-});
-
-document.getElementById('submitForm').addEventListener('click', function() {
-    alert('Form submitted successfully!');
-    window.location.href = 'page1.html';
-});
+function navigateToPage(page) {
+    window.location.href = page;
+}
